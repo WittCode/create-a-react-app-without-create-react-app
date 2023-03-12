db5937eee0179672f8bfab53e881572abc50f064
WittCode <wittcode@gmail.com>
2023-03-12 15:24:58 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Hello worlds!</h1>
+            </div>
+        );
+    }
+
+}
+
+export default App;
